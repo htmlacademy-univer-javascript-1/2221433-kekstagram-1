ce97f822caaed53026d1f04609ef2eba9c2b236a
Keks <keks@htmlacademy.ru>
2022-09-20 19:12:47 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,19 @@
+function getRandomNumber(from, to)
+{
+    if(from > to || from < 0 || to < 0)
+    {
+        return new Error('Error. Change input numbers');
+    } 
+   return Math.floor(Math.random() * to) + from;
+}
+
+function checkStringLength(str, maxLength)
+{
+    if (str.Length > maxLength)
+    {
+        return false;
+    }
+    return true;
+}
+getRandomNumber(2, 10);
+checkStringLength('Тест', 4);
